88d88511e00b77961917809ba376155b10192a44
TusharGupta03 <tushargupta0312@gmail.com>
2023-07-19 19:01:03 +0530

    Bugs

--- src/components/Home-Page/Home.js
+++ src/components/Home-Page/Home.js
@@ -4,7 +4,6 @@ import Navbar from './Navbar/Navbar';
 import Points from './Social-points/Points';
 import './Home.css'
 
-import { useState } from "react";
 export default function Home(props) {
 
   // setTimeout(() => {
