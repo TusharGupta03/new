a413d111cf6947d254b25ad3c517ddb7dd9ea075
TusharGupta03 <tushargupta0312@gmail.com>
2023-07-20 12:17:38 +0530

    Fixing Bugs

--- src/components/socket.js
+++ src/components/socket.js
@@ -1,4 +1,4 @@
 import { io } from 'socket.io-client';
-const socket = io("https://projectbackend-ycya.onrender.com")
+const socket = io("https://backend-50ji.onrender.com")
 
 export default socket
